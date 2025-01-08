4a52d1f0df02bb25f67baefdc3b42b88fff1ca5d
mlaguna <mlaguna@expediagroups.com>
2025-01-08 14:23:09 -0600

    404 error

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'docs',
+  distDir: 'build',
   /* config options here */
   reactStrictMode: false,
   images: {
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   assetPrefix: isProd ? 'https://elilv21.github.io/DemoApp/' : '',
-  basePath: isProd ? '/demo-app' : '',
+  basePath: isProd ? '/build' : '',
   env: {},
 };
 
